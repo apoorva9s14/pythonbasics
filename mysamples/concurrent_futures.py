5a73b559efe99a80aa85ef2d0b39a92242ebc413
apoorva9s14 <apoorvalaiti16@gmail.com>
2019-11-23 20:20:54 +0530

    profiling and pool executor

--- mysamples/concurrent_futures.py
+++ mysamples/concurrent_futures.py
@@ -1,22 +1,66 @@
-'''
+"""
 Simple ThreadPoolExecutor example
-'''
+"""
 
 import concurrent.futures
 import time
+import cProfile
+import pstats
+from profiling import decorator_profile
+
 
 def mysamplejob(i):
     '''I/O intense tasks'''
     time.sleep(10)
+    x = 1
+    time.sleep(30)
     return i
 
-def mytestfn():
-	'''sample fn to spawn threads'''
-	tpool = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-	futures = []
 
-	for i in range(0,10):
-		futures.append(tpool.submit(mysamplejob,i))
-	
-	for task in concurrent.futures.as_completed(futures):
-		print(task.result())
+def threadfn():
+    """sample fn to spawn threads"""
+    tpool = concurrent.futures.ThreadPoolExecutor(max_workers=8000)
+    #TODO - what is the upper limit of max_workers
+    futures = []
+    results = []
+
+    for i in range(0, 8000):
+        futures.append(tpool.submit(mysamplejob, i))
+
+    for task in concurrent.futures.as_completed(futures):
+        results.append(task.result())
+    # print("Results", results)
+
+
+def processjob():
+    tpool = concurrent.futures.ThreadPoolExecutor(max_workers=2000)
+    futures = []
+    results = []
+
+    for i in range(0, 2000):
+        futures.append(tpool.submit(mysamplejob, i))
+    for task in concurrent.futures.as_completed(futures):
+        results.append(task.result())
+    # print("Results",results)
+
+def processfn():
+    """sample fn to spawn processes"""
+    tpool = concurrent.futures.ProcessPoolExecutor(max_workers=4)
+    futures = []
+
+    for i in range(0, 4):
+            futures.append(tpool.submit(processjob))
+
+    for task in concurrent.futures.as_completed(futures):
+        # print(task.result())
+        pass
+
+
+if __name__ == "__main__":
+    # decorator_profile(threadfn)()
+    cProfile.run('processfn()', 'proessStats')
+    cProfile.run('threadfn()', 'threadStats')
+    p = pstats.Stats('proessStats')
+    print(p.sort_stats('time').print_stats(10))
+    p = pstats.Stats('threadStats')
+    print(p.sort_stats('time').print_stats(10))
--- mysamples/profiling.py
+++ mysamples/profiling.py
@@ -1,6 +1,25 @@
 '''to get profiling details for any function'''
+import cProfile
+import pstats
 
-import profile
 
-from concurrent_futures import mytestfn
-profile.run('print(mytestfn())')
+def decorator_profile(inputfn):
+    cProfile.run(inputfn.__name__ + '()')
+    return inputfn
+
+
+# def decorator_profile(inputfn):
+#     # cProfile.run(inputfn.__name__ + '()')
+#     def inner():
+#         cProfile.run(inputfn.__name__ + '()')
+#         return inputfn()
+#     return inner
+
+
+# from concurrent_futures import processfn, threadfn
+# if __name__ == "__main__":
+#     cProfile.run('processfn()', 'proessStats')
+#     cProfile.run('threadfn()', 'threadStats')
+#     p = pstats.Stats('proessStats')
+#     print(p.sort_stats('time').print_stats(10))
+
